20ad21f859c6c8f95436fc47f2781fe92fcbdb9a
Rick Cottle <richard.cottle@honeywell.com>
2018-10-16 17:23:25 -0700

    Created version that correctly flattens parameters, like was working in signals.

--- make_pccgraph.py
+++ make_pccgraph.py
@@ -59,6 +59,7 @@ nrows = sheet.nrows
 ncols = sheet.ncols
 
 signals_sheet = [] # An arrary of all signals filtered for fm_keywords
+signals_sheet_new = [] # An arrary of all signals remaining after flattenging
 llr_signals = {} # A dictionary of rows each signal exists in { signal: [llr_rows]}
 
 # Read signals spreadsheet and keep rows that have legit FM keywords, capture defines in dictionary llr_signals
@@ -76,7 +77,6 @@ for row in range(1,nrows):
         new_row += 1
 
 # For every llr_signal, look for multiples.  For those whose defined is Input, combine rows.  Write to new_data
-signals_sheet_new = [] # An arrary of all signals remaining after flattenging
 for signal in llr_signals:
     row_numbers = llr_signals[signal]
     if len(row_numbers) == 1:
@@ -126,8 +126,9 @@ sheet = book.sheet_by_name('HAM Parameter')
 nrows = sheet.nrows
 ncols = sheet.ncols
 
-params_sheet = [] # An arrary of all signals filtered for fm_keywords
-params_sheet_new = [] # An arrary of all signals remaining after flattenging
+params_sheet = [] # An arrary of all parameters filtered for fm_keywords
+params_sheet_new = [] # An arrary of all parameters remaining after flattenging
+llr_params = {} # A dictionary of rows each parameters exists in { signal: [llr_rows]}
 
 # Read params spreadsheet and keep rows that have legit FM keywords
 
@@ -135,18 +136,43 @@ new_row = 0
 header = sheet.row_values(0)
 for row in range(1,nrows):
     fm_keyword = sheet.cell(row,1).value
+    llr_param = sheet.cell(row,0).value
     if fm_keyword in fm_keywords:
         params_sheet.append(sheet.row_values(row))
+        if llr_param in llr_params:
+            llr_params[llr_param].append(new_row)
+        else:
+            llr_params[llr_param] = [new_row]
         new_row += 1
+
+# For every llr_param, look for multiples.  Combine rows.  Write to new_data
+for param in llr_params:
+    row_numbers = llr_params[param]
+    if len(row_numbers) == 1:
+        new_data = params_sheet[row_numbers[0]]  # Target data exists in row_data[0], only row for this signal
+    else:
+        pass
+        new_data = []  # New list to hold cell values we will write to a new row
+        used_functions = []  # List to hold the names of all functions that use this signal
+        row_data_list = []  # List to hold spreadsheet data for each of the rows being flattened
+        for row_number in row_numbers: # Store each of the rows to be flattened in this dictionary
+            row_data_list.append(params_sheet[row_number])
+        for row_data in row_data_list:  # Build new combined parameter info from each parameter based on rules
+            for function in row_data[2].split():
+                used_functions.append(function) # Collect all the functions that use the signal
+            new_data = row_data
+            new_data[2] = ' '.join(used_functions) # Build combined list of defined
+
+    params_sheet_new.append(new_data)
 
 # Write new Excel workbook for parameters
 book2 = xlwt.Workbook()
-sheet1 = book2.add_sheet('HAM Signal')
+sheet1 = book2.add_sheet('HAM Parameter')
 
 for col, heading in enumerate(header):
     sheet1.write(0, col, heading)
 
-for row, data in enumerate(params_sheet):
+for row, data in enumerate(params_sheet_new):
     for col, cell in enumerate(data):
         sheet1.write(row + 1, col, cell)
 
